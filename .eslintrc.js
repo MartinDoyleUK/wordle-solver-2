68ed206d0a405ddebe535e2045c9c917a626045c
Martin Doyle <martin.doyle@nutmeg.com>
2023-07-30 13:43:50 +0100

    Update eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,77 @@
 module.exports = {
   extends: 'canonical/auto',
+  overrides: [{
+    files: ['package.json'],
+    rules: {
+      // Specific sorting rules for the package.json file
+      'jsonc/sort-keys': [
+        2,
+        {
+          order: [
+            // Project information
+            'name',
+            'version',
+            'description',
+            'keywords',
+            'homepage',
+            'bugs',
+            'license',
+            'author',
+            'contributors',
+            'repository',
+            'funding',
+            'private',
+
+            // Technical usage information
+            'engines',
+            'engineStrict',
+            'os',
+            'cpu',
+            'publishConfig',
+            'workspaces',
+            'type',
+            'config',
+
+            // Output/build definitions
+            'files',
+            'directories',
+            'packageManager',
+            'exports',
+            'imports',
+            'main',
+            'browser',
+            'bin',
+            'man',
+
+            // Scripts
+            'scripts',
+
+            // Other configuration
+            'lint-staged',
+            'nutkit',
+
+            // Dependency related configuration
+            'dependencies',
+            'devDependencies',
+            'peerDependencies',
+            'peerDependenciesMeta',
+            'bundleDependencies',
+            'optionalDependencies',
+            'overrides',
+          ],
+          pathPattern: '^$',
+        },
+        {
+          order: { type: 'asc' },
+          pathPattern: '^(?:dev|peer|optional|bundle)?[Dd]ependencies(?:Meta)?$',
+        },
+      ],
+    }
+  }],
   rules: {
     'id-length': 0,
     'no-console': 0,
     'prettier/prettier': 0,
     'unicorn/prevent-abbreviations': 0,
-  },
+  }
 };
